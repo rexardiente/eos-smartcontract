b03a41efdbddbae584729139b8b650f9d93f90c4
Rex Ardiente <rexardiente09@gmail.com>
2020-05-14 18:57:45 +0800

    Update: Game Ranking

--- game.rank/game.rank.hpp
+++ game.rank/game.rank.hpp
@@ -4,19 +4,16 @@ using namespace eosio;
 
 class [[eosio::contract("game.rank")]] gamerank : public contract
 {
-public:
-  typedef __int128 uint128_t;
-  using contract::contract;
-  gamerank(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
   /* Game Rank Table
-   *
-   *  ID - Primary Key hostname.
-   *  game - Secondary key Game title.
-   *  platform - String name of the Platform.
-   *  symbol - symbol is member of an asset instance.
-   *  info - JSON string for list of top users [{user: 'name type', total_reward: 'asset', symbol: 'asset.symbol.raw'}].
-   *  created_at - timestamp datetime execution.
-   */
+  *
+  *  ID - Primary Key hostname.
+  *  game - Secondary key Game title.
+  *  platform - String name of the Platform.
+  *  symbol - symbol is member of an asset instance.
+  *  info - JSON string for list of top users [{user: 'name type', total_reward: 'asset', symbol: 'asset.symbol.raw'}].
+  *  created_at - timestamp datetime execution.
+  */
+private:
   struct [[eosio::table]] rank
   {
     name id;
@@ -27,12 +24,20 @@ public:
 
     auto primary_key() const { return id.value; }
     uint32_t secondary_key() const { return created_at; }
+
     EOSLIB_SERIALIZE(rank, (id)(platform)(game)(info)(created_at))
   };
+
   typedef eosio::multi_index<"ranks"_n, rank> rank_index;
+
+public:
+  using contract::contract;
+
+  gamerank(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
 
   // [[eosio::action]] void getbyid( name id);
   [[eosio::action]] void erase(name id);
+  [[eosio::action]] void byid(name id);
   [[eosio::action]] void create(name id,
                                 std::string game,
                                 std::string platform,
@@ -43,4 +48,8 @@ public:
                                 std::string platform,
                                 std::string info,
                                 uint32_t created_at);
+
+  using create_action = action_wrapper<"create"_n, &gamerank::create>;
+  using update_action = action_wrapper<"update"_n, &gamerank::update>;
+  using erase_action = action_wrapper<"erase"_n, &gamerank::erase>;
 };
