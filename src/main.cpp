c0849bcec66ff182aa32513b387c7d0e210bc21e
Brett Harvey <brettharvey2014@gmail.com>
2020-06-01 15:28:02 -0300

    UPDATE renamed contract, method on line 14.

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,19 @@
 
 #define user_name name
 
-// #include "game_files/game.rank/game.rank.hpp"
 #include "game_files/game.rank/game.rank.cpp"
 
-class [[eosio::contract("panels")]] panels : public eosio::contract
+class [[eosio::contract("main")]] main : public eosio::contract
 {
 private:
   gamerank gamerank_controller(name receiver, name code, datastream<const char *> ds);
 
 public:
   using contract::contract;
-  panels(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
+  main(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
 
-  //gamerank_controller(_self) {
-  	[[eosio::action]] void add(uint64_t id, std::vector<gamerank::user> data, uint64_t created_at)
-  	{
-    	gamerank_controller.add(id, data, created_at);
-  	}
-  //};
+  [[eosio::action]] void add(uint64_t id, std::vector<gamerank::user> data, uint64_t created_at)
+  {
+    gamerank_controller.add(id, data, created_at);
+  }
 };
