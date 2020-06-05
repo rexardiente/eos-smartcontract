d436e58991c831a523683e8e4dd4e6b93c04b04d
Brett Stefanishin <shelleymacpherson@Shelleys-MacBook-2.local>
2020-06-05 02:06:34 -0300

    added more game.rank functions.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -10,7 +10,9 @@ public:
   using contract::contract;
   main(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
 
-  [[eosio::action]] void del(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  /* ----- GAME-RELATED FUNCTIONS AND ACTIONS ----- */ 
+  /* -- GAME.RANK DELETE -- */
+    [[eosio::action]] void del(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
   {
     // Check if the user is authorized to action
     check(has_auth(get_self()), "User with admin access only authorized.");
@@ -18,5 +20,27 @@ public:
 
     gamerank::del_action delaction("game.rank"_n, {get_self(), "active"_n});
     delaction.send(id);
+  };
+
+  /* -- GAME.RANK ADD -- */
+    [[eosio::action]] void add(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    gamerank::add_action addaction("game.rank"_n, {get_self(), "active"_n});
+    addaction.send(id, data, createdAt);
+  };
+
+  /* -- GAME.RANK EDIT -- */
+  [[eosio::action]] void edit(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    gamerank::edit_action editaction("game.rank"_n, {get_self(), "active"_n});
+    editaction.send(id, data, createdAt);
   };
 };
