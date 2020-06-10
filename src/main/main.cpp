78c0025b21037e0fc61b8dcfcdc4430f73949a06
Brett Stefanishin <shelleymacpherson@Shelleys-MacBook-2.local>
2020-06-10 05:17:26 -0300

    UPDATE main.cpp, ADD main.test.cpp, ADD README.md to /main directory

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,173 +5,58 @@
 
 using namespace eosio;
 
-class [[eosio::contract("main")]] main : public contract
-{
-
+class [[eosio::contract("main")]] main : public contract {
 public:
   using contract::contract;
   main(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
 
-  /* ----- GAME-RELATED FUNCTIONS AND ACTIONS ----- */
-  // GAMES.CPP
-  // @abi action
-  /* -- Add Configuration  -- */
-  [[eosio::action]] void addconfig(uint64_t id)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
+void gamerank::add(uint64_t id, std::vector<user> data, uint64_t created_at)
+{
+  // Check if the user is authorized to action
+  check(has_auth(get_self()), "User with admin access only authorized.");
+  require_auth(get_self());
 
-    games::add_config addconfig("games"_n, {get_self(), "active"_n});
-    addconfig.send(id);
-  };
+  rank_index gamerank(get_self(), get_self().value);
+  auto itr = gamerank.find(id);
+  check(itr == gamerank.end(), "Rank already exist.");
 
-  // @abi action
-  /* -- Edit Configuration  -- */
-  [[eosio::action]] void editconfig(uint64_t id)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
+  gamerank.emplace(get_self(), [&](auto &row) {
+    row.id = id;
+    row.data = data;
+    row.created_at = created_at;
+  });
+};
 
-    games::edit_config editconfig("games"_n, {get_self(), "active"_n});
-    editconfig.send(id);
-  };
+void gamerank::edit(uint64_t id, std::vector<user> data, uint64_t created_at)
+{
+  // Check if the user is authorized to action
+  check(has_auth(get_self()), "User with admin access only authorized.");
+  require_auth(get_self());
 
-  // @abi action
-  /* -- Delete Configuration  -- */
-  [[eosio::action]] void delconfig(uint64_t id)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
+  rank_index gamerank(get_self(), get_self().value);
+  auto itr = gamerank.find(id);
+  check(itr != gamerank.end(), "Rank doesn't exist.");
 
-    games::del_config delconfig("games"_n, {get_self(), "active"_n});
-    delconfig.send(id);
-  };
+  gamerank.modify(itr, get_self(), [&](auto &row) {
+    row.id = id;
+    row.data = data;
+    row.created_at = created_at;
+  });
+};
 
-  // @abi action
-  /* -- Add Action -- */
-  [[eosio::action]] void add(uint64_t id, std::string title, std::string description)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
+void gamerank::del(uint64_t id)
+{
+  // Auth Validation
+  check(has_auth(get_self()), "User with admin access only authorized.");
+  require_auth(get_self());
 
-    games::add_action addaction("games"_n, {get_self(), "active"_n});
-    addaction.send(id, title, description);
-  };
+  rank_index gamerank(get_self(), get_self().value);
 
-  // @abi action
-  /* -- Edit Action  -- */
-  [[eosio::action]] void edit(uint64_t id, std::string title, std::string description)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
+  // Check if game exits
+  auto itr = gamerank.find(id);
+  check(itr != gamerank.end(), "Rank doesn't exits.");
 
-    games::edit_action editaction("games"_n, {get_self(), "active"_n});
-    editaction.send(id, title, description);
-  };
-
-  // @abi action
-  /* -- Delete Action  -- */
-  [[eosio::action]] void del(uint64_t id)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    games::del_action delaction("games"_n, {get_self(), "active"_n});
-    delaction.send(id);
-  };
-
-  // GAME RANK
-  // @abi action
-  /* -- Add Action -- */
-  [[eosio::action]] void gamerank_add(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    gamerank::add_action addaction("game.rank"_n, {get_self(), "active"_n});
-    addaction.send(id, data, createdAt);
-  };
-
-  // @abi action
-  /* -- Edit Action -- */
-  [[eosio::action]] void gamerank_edit(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    gamerank::edit_action editaction("game.rank"_n, {get_self(), "active"_n});
-    editaction.send(id, data, createdAt);
-  };
-
-  // @abi action
-  /* -- Delete Action -- */
-  [[eosio::action]] void gamerank_del(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    gamerank::del_action delaction("game.rank"_n, {get_self(), "active"_n});
-    delaction.send(id);
-  };
-
-  // OVERALL.CPP
-  // @abi action
-  /* -- Add Action(Overall)  -- */
-  [[eosio::action]] void add_overall(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    overall::add_action addaction("overall"_n, {get_self(), "active"_n});
-    addaction.send(id, data, createdAt);
-  };
-
-  /* -- Edit Action(Overall)   -- */
-  /*
-  [[eosio::action]] void edit_overall(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    overall::edit_action edit_overall("overall"_n, {get_self(), "active"_n});
-    edit_overall.send(id, data, createdAt);
-  };
-*/
-  /* -- Edit Action (Overall)  -- */
-  /*
-  [[eosio::action]] void del_overall(uint64_t id)
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    overall::del_action del_overall("overall"_n, {get_self(), "active"_n});
-    del_overall.send(id);
-  };
-  */
-
-  // TOKEN CONTRACT
-  /* -- Add Action -- */
-  /*
-  [[eosio::action]] void create(const name& issuer, const asset&  max_supply);
-  {
-    // Check if the user is authorized to action
-    check(has_auth(get_self()), "User with admin access only authorized.");
-    require_auth(get_self());
-
-    donuttoken::create addaction("game.rank"_n, {get_self(), "active"_n});
-    addaction.send(id, data, createdAt);
-  };
-  */
+  // If all validations successful, remove the game from the list
+  gamerank.erase(itr);
+};
 };
---
+++ src/main/main.test.cpp
@@ -0,0 +1,178 @@
+#include <eosio/eosio.hpp>
+#include "../game_files/game.rank/game.rank.hpp"
+#include "../game_files/games/games.hpp"
+#include "../game_files/overall/overall.hpp"
+
+using namespace eosio;
+
+class [[eosio::contract("main")]] main : public contract
+{
+
+public:
+  using contract::contract;
+  main(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
+
+  /* ----- GAME-RELATED FUNCTIONS AND ACTIONS ----- */
+  // GAMES.CPP
+  // @abi action
+  /* -- Add Configuration  -- */
+  [[eosio::action]] void addconfig(uint64_t id)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    games::add_config addconfig("games"_n, {get_self(), "active"_n});
+    addconfig.send(id);
+  };
+
+  // @abi action
+  /* -- Edit Configuration  -- */
+  [[eosio::action]] void editconfig(uint64_t id)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    games::edit_config editconfig("games"_n, {get_self(), "active"_n});
+    editconfig.send(id);
+  };
+
+  // @abi action
+  /* -- Delete Configuration  -- */
+  [[eosio::action]] void delconfig(uint64_t id)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    games::del_config delconfig("games"_n, {get_self(), "active"_n});
+    delconfig.send(id);
+  };
+
+  // @abi action
+  /* -- Add Action -- */
+  [[eosio::action]] void add(uint64_t id, std::string title, std::string description)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    games::add_action addaction("games"_n, {get_self(), "active"_n});
+    addaction.send(id, title, description);
+  };
+
+  // @abi action
+  /* -- Edit Action  -- */
+  [[eosio::action]] void edit(uint64_t id, std::string title, std::string description)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    games::edit_action editaction("games"_n, {get_self(), "active"_n});
+    editaction.send(id, title, description);
+  };
+
+  // @abi action
+  /* -- Delete Action  -- */
+  [[eosio::action]] void del(uint64_t id)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    games::del_action delaction("games"_n, {get_self(), "active"_n});
+    delaction.send(id);
+  };
+
+  // GAME RANK
+  // @abi action
+  /* -- Add Action -- */
+  [[eosio::action]] void add(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    gamerank::add_action addaction("game.rank"_n, {get_self(), "active"_n});
+    addaction.send(id, data, createdAt);
+  };
+
+  // @abi action
+  /* -- Edit Action -- */
+  [[eosio::action]] void edit(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    gamerank::edit_action editaction("game.rank"_n, {get_self(), "active"_n});
+    editaction.send(id, data, createdAt);
+  };
+
+  // @abi action
+  /* -- Delete Action -- */
+  [[eosio::action]] void del(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    gamerank::del_action delaction("game.rank"_n, {get_self(), "active"_n});
+    delaction.send(id);
+  };
+
+  // OVERALL.CPP
+  // @abi action
+  /* -- Add Action(Overall)  -- */
+  /*
+  [[eosio::action]] void overall_add(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    overall::add_action addaction("overall"_n, {get_self(), "active"_n});
+    addaction.send(id, data, createdAt);
+  };
+*/
+  /* -- Edit Action(Overall)   -- */
+  /*
+  [[eosio::action]] void edit_overall(uint64_t id, std::vector<gamerank::user> data, uint64_t createdAt)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    overall::edit_action edit_overall("overall"_n, {get_self(), "active"_n});
+    edit_overall.send(id, data, createdAt);
+  };
+*/
+  /* -- Edit Action (Overall)  -- */
+  /*
+  [[eosio::action]] void del_overall(uint64_t id)
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    overall::del_action del_overall("overall"_n, {get_self(), "active"_n});
+    del_overall.send(id);
+  };
+  */
+
+  // TOKEN CONTRACT
+  /* -- Add Action -- */
+  /*
+  [[eosio::action]] void create(const name& issuer, const asset&  max_supply);
+  {
+    // Check if the user is authorized to action
+    check(has_auth(get_self()), "User with admin access only authorized.");
+    require_auth(get_self());
+
+    donuttoken::create addaction("game.rank"_n, {get_self(), "active"_n});
+    addaction.send(id, data, createdAt);
+  };
+  */
+};
