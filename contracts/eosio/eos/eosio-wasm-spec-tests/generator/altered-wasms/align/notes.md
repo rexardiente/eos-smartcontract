382f470d378d523c4a927897148a8852ea6b2abd
= <=>
2020-07-16 00:09:34 -0300

    ADDED TH contract

--- contracts/eosio/eos/eosio-wasm-spec-tests/generator/altered-wasms/align/notes.md
+++
@@ -1,4 +0,0 @@
-## Changes
-- align.*:
-    - Change to start with one page of memory
-        - This is necessary because of the data section added by our test harness setup.
---
+++ src/game_files/treasure.hunt/treasure.hunt.cpp
@@ -0,0 +1,55 @@
+/*
+--------------------------------------------------------------
+Filename: treasure.hunt.cpp
+Purpose: this file contains tables and functions for the TH game.
+--------------------------------------------------------------
+*/
+
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract("treasure.hunt")]] treasurehunt : public contract {
+  private:
+    struct [[eosio::table]] user_info {
+      eosio::uint16_t user_identifier;
+      eosio::name username;
+      eosio::asset user_balance;
+      uint64_t primary_key() const { return user_identifier; }
+    };
+
+    struct [[eosio::table]] games_info {
+      eosio::uint16_t game_identifier;
+      eosio::name game_title;
+      eosio::asset game_balance;
+      uint64_t primary_key() const { return game_identifier; }
+    };
+
+  public:
+    treasurehunt(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+    void loguser(username user,uint16_t user_identifier,name username,asset user_balance) {
+      user_index userlog(get_first_receiver(), get_first_receiver().value);
+      require_auth(user);
+
+      auto iter=userlog.find(user);
+      if(iter==userlog.end()) {
+        userlog.emplace(user, [&]( auto& row ) {
+          row.key = user;
+          row.user_identifier =user_identifier;
+          row.username = username;
+          row.user_balance = user_balance;
+        });
+      } else {
+        uint64_t getuser;
+        getuser=iter->username;
+        print("data already exist ",name{getuser}," \t");
+      }
+    }
+    void getBalance(username owner){
+      eosio::token t(N(eosio.token));
+      const auto sym_name = eosio::symbol_type(S(4,EOS)).name();
+      const auto my_balance = t.get_balance(N(owner), sym_name );
+      eosio::print("My balance is ", my_balance);
+    }
+};
