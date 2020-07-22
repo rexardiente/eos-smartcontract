bd24fc3228672076c044f0b533d28d56aefa927d
tata-prog <65842297+tata-prog@users.noreply.github.com>
2020-07-22 14:12:20 +0800

    Update treasure.hunt.hpp

--- src/game_files/treasure.hunt/treasure.hunt.hpp
+++ src/game_files/treasure.hunt/treasure.hunt.hpp
@@ -1,46 +1,112 @@
 #include <eosio/eosio.hpp>
+#include <eosio/system.hpp>
 
+#include <eosio/asset.hpp>
 using namespace std;
 using namespace eosio;
-class [[eosio::contract("treasure.hunt")]] treasurehunt : public contract {
+class [[eosio::contract("treasurehunt")]] treasurehunt : public contract {
 
   private:
-    struct [[eosio::table]] user_info {
-      uint16_t user_identifier;
-      name username;
-      asset user_balance;
-      auto primary_key() const { return username.value; }
+  enum map_type: int8_t {
+	EMPTY = 0,
+  MAP_A = 1,
+	MAP_B = 2,
+	MAP_C = 3,
+  VOID = 4
+};
+struct island {
+      uint8_t type;
+      uint8_t attack_point;
     };
-
-    struct [[eosio::table]] map_info {
-      uint16_t map_identifier;
-      name map_name;
-
-      uint64_t primary_key() const { return map_identifier; }
+    
+const map<uint8_t, island> map_dict = {
+      { 0, {EMPTY, 0} },       
+      { 1, {MAP_A, 1} },
+      { 2, {MAP_A, 1} },
+      { 3, {MAP_A, 2} },
+      { 4, {MAP_A, 2} },
+      { 5, {MAP_A, 3} },
+      { 6, {MAP_B, 1} },
+      { 7, {MAP_B, 1} },
+      { 8, {MAP_B, 2} },
+      { 9, {MAP_B, 2} },
+      { 10, {MAP_B, 3} }, 
+      { 11, {MAP_C, 1} },
+      { 12, {MAP_C, 1} },
+      { 13, {MAP_C, 2} },
+      { 14, {MAP_C, 2} },
+      { 15, {MAP_B, 3} },
+      { 16, {VOID, 0} }
     };
-
-    typedef eosio::multi_index<name("maps"), map_info> maps_table;
-
-    maps_table _maps;
-
-
-    enum game_status: int8_t {
+    
+   
+enum game_status: int8_t {
       ONGOING     = 0,
       PLAYER_WON   = 1,
       PLAYER_LOST  = -1
     };
 
+
+  struct game {
+    
+      int8_t          ticket_player = 1;
+      int8_t          ticket_ai = 1;
+      vector<uint8_t> map_player = {1, 2, 3,4,7,8,9,10,11,12,13,14,15,16};   
+      vector<uint8_t> map_ai = {1, 2, 3,4,7,8,9,10,11,12,13,14,15,16}; 
+      vector<uint8_t> hand_player = {0, 0, 0,0};   
+      vector<uint8_t> hand_ai = {0, 0, 0,0};   
+      uint8_t         selected_map_player = 0;             
+      uint8_t         selected_map_ai = 0;   
+      uint8_t         ticket_lost_player = 0;
+      uint8_t         ticket_lost_ai = 0;
+      int8_t          status = ONGOING;
+    };
+
+    struct [[eosio::table]] user_info {
+      uint16_t user_identifier;
+      name username;
+      asset user_balance;
+      game game_data;
+      auto primary_key() const { return username.value; }
+    };
+struct [[eosio::table]] seed {
+      uint64_t        key = 1;
+      uint32_t        value = 1;
+
+      auto primary_key() const { return key; }
+    };
+    
+
+
+    
+    typedef eosio::multi_index<name("seed"), seed> seed_table;
     typedef eosio::multi_index<name("users"), user_info> users_table;
-
     users_table _users;
+    seed_table _seed;
+    void draw_one_map(vector<uint8_t>& mapl, vector<uint8_t>& hand);
+   
+    int ai_choose_map(const game& game_data);
+    //int calculate_attack_point(const island& island1, const island& island2);
+    int calculate_attack_point(const island& island1, const island& island2);
+    void resolve_selected_maps(game& game_data);
+    void update_game_status(user_info& user);
+     int calculate_ai_map_score(const int strategy_idx, const int8_t ticket_ai, 
+                                const island& ai_map, const vector<uint8_t> hand_player);
 
+
+    int ai_best_map_win_strategy(const int ai_attack_point, const int player_attack_point);
+    int ai_min_loss_strategy(const int ai_attack_point, const int player_attack_point);
+    int ai_points_tally_strategy(const int ai_attack_point, const int player_attack_point);
+    int ai_loss_prevention_strategy(const int8_t ticket_ai, const int ai_attack_point, const int player_attack_point);
+    int random(const int range);
+    
   public:
     treasurehunt( name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds),
                        _users(receiver, receiver.value),
                        _seed(receiver, receiver.value) {}
 
     [[eosio::action]]
-    void login(name username);
+    void loguser(name username);
 
     [[eosio::action]]
     void startgame(name username);
@@ -49,5 +115,7 @@ class [[eosio::contract("treasure.hunt")]] treasurehunt : public contract {
     void endgame(name username);
 
     [[eosio::action]]
-    void nextround(name username)
+    void nextround(name username);
+    [[eosio::action]]
+    void playhunt(name username, uint8_t player_map_idx);
 };
