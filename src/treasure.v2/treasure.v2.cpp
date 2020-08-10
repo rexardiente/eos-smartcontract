f81abec978da28bda9c2e3583fca1d9b5dda8e41
rex ardiente <rexardiente09@gmail.com>
2020-08-10 10:10:05 +0800

    Create treasure.v2.cpp

---
+++ src/treasure.v2/treasure.v2.cpp
@@ -0,0 +1,15 @@
+#include "gameplay.cpp"
+
+void treasurev2::hello(name username) {
+    // print("Hello:", username.value);
+    int genNum = rng(10);
+    print(genNum);
+}
+
+void treasurev2::login(name username) {};
+
+void treasurev2::startgame(name username) {};
+
+void treasurev2::end(name username) {};
+
+void treasurev2::reset(name username) {};
