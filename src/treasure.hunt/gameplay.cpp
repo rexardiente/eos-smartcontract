050f8b354c8f80359be192ac0dfc937178633d03
rex ardiente <rexardiente09@gmail.com>
2020-09-03 11:57:33 +0800

    Fix: contract with no actions and trying to create dispatcher

--- src/treasure.hunt/gameplay.cpp
+++
@@ -1,1 +0,0 @@
-#include "treasure.hunt.hpp"
--- src/treasure.hunt/treasure.hunt.hpp
+++ src/treasure.hunt/treasure.hunt.hpp
@@ -1,13 +1,5 @@
-/*
---------------------------------------------------------------
-Filename: treasurehunt.hpp
-Purpose: This is a header file for the TH game contract.
---------------------------------------------------------------
-*/
-
 #include <eosio/eosio.hpp>
 #include <eosio/system.hpp>
-
 #include <eosio/asset.hpp>
 using namespace std;
 using namespace eosio;
@@ -56,19 +48,29 @@ private:
     PLAYER_LOST = -1
   };
 
+  enum set_sail : int8_t
+  {
+    NOT_READY = 0,
+    READY = 1
+  };
+
+  enum newDestination : int8_t
+  {
+    NO = 0,
+    YES = 1
+  };
+
   struct game
   {
     int8_t ticket_player = 1;
-    int8_t ticket_ai = 1;
     vector<uint8_t> map_player = {1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    vector<uint8_t> map_ai = {1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     vector<uint8_t> hand_player = {0, 0, 0, 0};
-    vector<uint8_t> hand_ai = {0, 0, 0, 0};
     uint8_t selected_map_player = 0;
-    uint8_t selected_map_ai = 0;
     uint8_t ticket_lost_player = 0;
-    uint8_t ticket_lost_ai = 0;
     int8_t status = ONGOING;
+    int8_t setsail = NOT_READY;
+    int8_t newdestination = NO;
+    int16_t explorers = 0;
   };
 
   struct [[eosio::table]] user
@@ -77,7 +79,6 @@ private:
     name username;
     asset user_balance;
     game game_data;
-
     auto primary_key() const { return username.value; }
   };
 
@@ -85,66 +86,60 @@ private:
   {
     uint16_t ticket_ID;
     name ticket_name;
-
-    auto primary_key() const { return ticket_ID; }
-  };
-
-  struct [[eosio::table]] history
-  {
-    uint16_t ticket_ID;
-    name ticket_name;
-    asset user_balance;
-    game game_data;
-
-    auto primary_key() const { return ticket_ID; }
+    auto primary_key() const { return ticket_name.value; }
   };
 
   struct [[eosio::table]] seed
   {
     uint64_t key = 1;
     uint32_t value = 1;
-
     auto primary_key() const { return key; }
   };
 
-  using seed_table = eosio::multi_index<"seed"_n, seed>;
-  using users_table = eosio::multi_index<"user"_n, user>;
-  using ticket_table = eosio::multi_index<"ticket"_n, ticket>;
-  using history_table = eosio::multi_index<"history"_n, history>;
+  typedef eosio::multi_index<name("seed"), seed> seed_table;
+  typedef eosio::multi_index<name("user"), user> users_table;
+  typedef eosio::multi_index<name("ticket"), ticket> ticket_table;
 
-  users_table _user;
+  users_table _users;
   seed_table _seed;
-  ticket_table _ticket;
-  history_table _history;
-
-  /* --- Common Actions --- */
-  void generate_panel();
-  void system_fairness_strategy();
-  int calculate_map_coins();
-  
-  int calculate_attack_point(const island &island1, const island &island2);
-  void resolve_selected_maps(game & game_data);
-  void update_game_status(user & user);
-  void new_destination(name username);
-
-  bool has_existing_game(name username);
-  void new_explorers(name username, uint16_t number_of_explorers);
-  void game_history(name & username);
-  void reset_game(name username);
+  ticket_table _tickets;
 
   /* RNG function */
   int random(const int range);
 
+  /* --- Common Actions --- */
+  void calculatePrize(name username, uint64_t results);
+  void generatePrize(name username, uint8_t selected_map_player);
+  bool hasexisting(user username);
+
 public:
   treasurehunt(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
-                                                                        _user(receiver, receiver.value),
+                                                                        _users(receiver, receiver.value),
                                                                         _seed(receiver, receiver.value),
-                                                                        _ticket(receiver, receiver.value),
-                                                                        _history(receiver, receiver.value) {}
+                                                                        _tickets(receiver, receiver.value) {}
 
   /* --- Authenticated Actions --- */
-  [[eosio::action]] void login(name username);
-  [[eosio::action]] void startgame(name username);
+  [[eosio::action]] void loguser(name username);
+
+  [[eosio::action]] void startgame(name username, uint8_t selected_map_player);
+
   [[eosio::action]] void endgame(name username);
+
+  [[eosio::action]] bool hasexisting(name username);
+
+  [[eosio::action]] void resetgame(name username);
+
+  [[eosio::action]] void destination(name username);
+
+  [[eosio::action]] void gamehistory(name username);
+
+  [[eosio::action]] void gamestatus(name username);
+
+  [[eosio::action]] void setsail(name username);
+
+  [[eosio::action]] void newexplorers(name username, uint16_t number_of_explorers);
+
+  [[eosio::action]] void nextround(name username);
+
   [[eosio::action]] void playhunt(name username, uint8_t player_map_idx);
 };
